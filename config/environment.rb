f0dd94c073b1c81c15986ae441eaee54a1cd3c86
Rodrigo <rodrigo.ulloa22@gmail.com>
2020-11-07 13:18:37 -0300

    quit the [] enviroment

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,14 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+user_name = Rails.application.credentials.send_grid_client[:sendgrid_username]
+password = Rails.application.credentials.send_grid_client[:sendgrid_password]
 ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => Rails.application.credentials.send_grid_client[:sendgrid_username],
-  :password => Rails.application.credentials.send_grid_client[:sendgrid_password],
+  :user_name => user_name,
+  :password => password,
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
